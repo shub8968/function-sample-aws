d0d27b31052cd5ec875004c11e96ba8a7ac56c20
Shubham Agrawal <shub8968@gmail.com>
2023-06-03 23:05:06 +0530

    Update FunctionConfiguration.java

--- src/main/java/example/FunctionConfiguration.java
+++ src/main/java/example/FunctionConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class FunctionConfiguration {
 	public static void main(String[] args) {
-        SpringApplication.run(CloudFunctionApplication.class, args);
+        //SpringApplication.run(CloudFunctionApplication.class, args);
     }
     @Bean
     public Function<String, String> reverseString() {
